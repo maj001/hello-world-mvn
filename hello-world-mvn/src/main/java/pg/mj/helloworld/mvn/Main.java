9e528771f7654f49dcf438c2e6ac11808139439b
maj001 <maj001@users.noreply.github.com>
2017-02-22 13:12:34 +1100

    workspace2: comment removed, exlamation mark added

--- hello-world-mvn/src/main/java/pg/mj/helloworld/mvn/Main.java
+++ hello-world-mvn/src/main/java/pg/mj/helloworld/mvn/Main.java
@@ -3,8 +3,7 @@ package pg.mj.helloworld.mvn;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World Maven");
+		System.out.println("Hello World Maven!");
 	}
 
 }
