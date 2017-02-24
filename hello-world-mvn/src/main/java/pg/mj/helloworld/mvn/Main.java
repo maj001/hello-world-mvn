e45e434c3d39877abb89e20a17408c7afc43143f
maj001 <maj001@users.noreply.github.com>
2017-02-25 10:34:00 +1100

    synchronized remote 'workspace2: line 1028' and added 'workspace1: line 1032'

--- hello-world-mvn/src/main/java/pg/mj/helloworld/mvn/Main.java
+++ hello-world-mvn/src/main/java/pg/mj/helloworld/mvn/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// workspace1: line 1024
+		// workspace1: line 1032
 		System.out.println("Hello World Maven!");
+		// workspace2: line 1028
 	}
 
 }
